66daef942f2520c37dd8a8f0c983dfd876565d93
Lawrence AbuDubai <EstarRave203@gmail.com>
2021-09-27 13:10:12 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationResult: {},
+      searchQuery: '',
+      showLocInfo: false,
+      showError: false
+    }
+  }
+
+  getLocFun = async (e) => {
+    e.preventDefault();
+    console.log('inside getLocFun')
+
+    // let cityName = e.target.city.value;
+    await this.setState({
+      searchQuery: e.target.city.value
+    })
+
+    console.log('key', process.env.REACT_APP_LOCATIONIQ_KEY);
+
+    try {
+      let reqUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchQuery}&format=json`;
+
+      let locResult = await axios.get(reqUrl);
+      console.log('aaaaaaaaaaa', locResult);
+      console.log('bbbbbbbb', locResult.data);
+      console.log('cccccccc', locResult.data[0]);
+      this.setState({
+        locationResult: locResult.data[0],
+        showLocInfo: true,
+        showError:false
+      })
+    } catch {
+      console.log('something went wrong')
+      this.setState({
+        showError: true,
+        showLocInfo:false
+      })
+    }
+
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h3>City Explorer app</h3>
+        {/* <button onClick={this.getLocFun}>Get Location</button> */}
+        <form onSubmit={this.getLocFun} >
+          <input type="text" name='city' />
+          <input type="submit" value='get city info' />
+        </form>
+
+        {this.state.showLocInfo &&
+          <>
+            <p>City name: {this.state.searchQuery}</p>
+            <p>latitude: {this.state.locationResult.lat}</p>
+            <p>longitude: {this.state.locationResult.lon} </p>
+
+            <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.locationResult.lat},${this.state.locationResult.lon}&zoom=10`} alt="city" />
+
+          </>
+        }
+
+        {this.state.showError &&
+          <p>Something went wrong in getting location data</p>
+        }
+
+
+
+      </div>
+    )
+  }
 }
 
 export default App;
